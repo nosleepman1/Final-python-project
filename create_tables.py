f7f3ef661542ed79c7a8252a682500c1a5bc441f
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-08-16 13:11:49 +0000

    chore(structure): suppression des doublons create_tables et insert_test_data a la racine

--- create_tables.py
+++
@@ -1,5 +0,0 @@
-# create_tables.py
-from database.create_tables import creer_tables
-
-if __name__ == "__main__":
-    creer_tables()
--- insert_test_data.py
+++
@@ -1,5 +0,0 @@
-# insert_test_data.py
-from database.insert_test_data import inserer_donnees_test
-
-if __name__ == "__main__":
-    inserer_donnees_test()
